4ad477f7214d1b2ea579e4406fe0c02eb0bf0e12
tomer <Tomer.Yaacoby@mail.huji.ac.il>
2023-06-06 15:41:50 +0300

    moderate activity

--- data_processor.py
+++ data_processor.py
@@ -27,7 +27,7 @@ class DataProcessor:
         anxiety_ICD10_col = self.data_analyser.get_anxiety_ICD10()
         self.new_data['anxiety_ICD10'] = anxiety_ICD10_col.astype(int)
         alchohol_ICD10_col = self.data_analyser.get_alchohol_ICD10()
-        self.new_data['alchohol_ICD10'] = alchohol_ICD10_col.astype(int)
+        self.new_data['alcohol_ICD10'] = alchohol_ICD10_col.astype(int)
         alzheimer_ICD10_col = self.data_analyser.get_alzheimer_ICD10()
         self.new_data['alzheimer_ICD10'] = alzheimer_ICD10_col.astype(int)
         gingivitis_ICD10_col = self.data_analyser.get_gingivitis_ICD10()
@@ -53,8 +53,21 @@ class DataProcessor:
             self.add_column_to_new_data(col)
         self.add_years_from_rec_to_diagnosis()
         self.clean_nan_values()
+        self.clean_duplicate_columns()
         self.clean_EPOE_and_years_values()
 
+    def clean_duplicate_columns(self):
+        self.new_data['Alzheimer_Diag'] = self.new_data.apply(lambda row: 1 if row['alzheimer_ICD10'] == 1 or row['Alzheimer_Diag'] == 1 else 0, axis=1)
+        self.new_data['Alcohol_Diag'] = self.new_data.apply(lambda row: 1 if row['alcohol_ICD10'] == 1 or row['Alcohol_Diag'] == 1 else 0, axis=1)
+        self.new_data['InsulinMedications'] = self.new_data.apply(lambda row: 1 if row['InsulinMedications'] == 1 or row['MedsInsulin'] == 1 else 0, axis=1)
+        self.new_data['CardioMedications'] = self.new_data.apply(lambda row: 1 if row['CardioMedications'] == 1 or row['MedsCardiovascular'] == 1 else 0, axis=1)
+        self.new_data['Moderate_Activity'] = self.get_final_moderate_activity_column()
+
+    def get_final_moderate_activity_column(self):
+        moderates_duration = self.new_data['DurationModerateActivity'].apply(lambda x: 1 if x > 40 else 0)
+        walkings = self.new_data["WalkingActivity_NumDaysWeek"].apply(lambda x: 1 if x > 6 else 0)
+        moderates_activity = self.new_data['ModeratePhysicalActivity_NumDaysWeek'].apply(lambda x: 1 if x > 4 else 0)
+        return moderates_duration + moderates_activity + walkings + self.new_data['DurationHeavyDIY']
     def clean_EPOE_and_years_values(self):
         # remove all the rows with EPOE = 1
         self.new_data['BiologicalSex'] = self.new_data['BiologicalSex'].apply(lambda s: 1 if s == 'Female' else 0)
@@ -67,8 +80,12 @@ class DataProcessor:
         diagnosed_combinations = diagnosed_data.groupby(['BirthYear', 'BiologicalSex']).size().reset_index().rename(columns={0: 'count'})
         matched_negatives = non_diagnosed_data.merge(diagnosed_combinations, on=['BirthYear', 'BiologicalSex'], how='inner')
         matched_lines = self.get_matched_lines(diagnosed_combinations, matched_negatives)
-        self.new_data = pd.concat([diagnosed_data, matched_lines]).drop(["years_from_rec_to_diagnosis", "count", "BirthYear", 'BiologicalSex', 'ICD10_Diags'], axis=1)
-        b=7
+        self.new_data = pd.concat([diagnosed_data, matched_lines]).drop(
+            ["years_from_rec_to_diagnosis", "count", "BirthYear", 'BiologicalSex',
+             'ICD10_Diags', 'alzheimer_ICD10', 'alcohol_ICD10', 'MedsInsulin',
+             'MedsCardiovascular', 'DurationHeavyDIY', 'DurationModerateActivity',
+             'WalkingActivity_NumDaysWeek','ModeratePhysicalActivity_NumDaysWeek',
+             ], axis=1)
 
     def get_matched_lines(self, diagnosed_combinations, matched_negatives):
         matched_lines = pd.DataFrame()
